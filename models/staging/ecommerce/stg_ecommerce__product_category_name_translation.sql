8ce4d26d536ca0b55307e02263ffb0838763675a
mtla-saurabh <89142371+mtla-saurabh@users.noreply.github.com>
2022-08-04 09:53:02 -0400

    fix: Customers.yml file included; chore: comments and end of line in files

--- models/staging/ecommerce/stg_ecommerce__product_category_name_translation.sql
+++ models/staging/ecommerce/stg_ecommerce__product_category_name_translation.sql
@@ -11,5 +11,5 @@ select
     string_field_1 as product_category_name_english
 
 from {{source('ecommerce', 'product_category_name_translation')}}
-
+-- where clause added to exclude a row containing header label
 where string_field_0!="product_category_name"
